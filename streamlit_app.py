417ae619db70996e0d9e65aec3770a70678d5065
lucyyangg <105331711+lucyyangg@users.noreply.github.com>
2023-12-07 07:47:25 -0700

    Update streamlit_app.py
    filter df based on selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,10 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit), ['Avocado'
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
+
+# Let's put a lick list here so they can pick the fruits they want to include
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+#display the table on the page
+streamlit.dataframe(fruits_to_show)
